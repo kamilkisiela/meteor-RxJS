4dd617a34c1294fe47fb25e07ffb3d8e9c16f912
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2015-12-02 19:53:11 +0100

    chore: add support for meteor

---
+++ package.js
@@ -0,0 +1,17 @@
+Package.describe({
+  name: "mys:rx",
+  summary: "Library for composing asynchronous and event-based operations in JavaScript",
+  version: "4.0.7",
+  documentation: 'readme.md',
+  git: 'https://github.com/kamilkisiela/meteor-RxJS.git'
+});
+
+Package.onUse(function (api) {
+
+  api.versionsFrom("METEOR@1.0");
+
+  api.addFiles([
+    'dist/rx.all.js'
+  ], 'client');
+
+});
